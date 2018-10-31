aaa13b4f66a8c6985da10f9b987f1b1a8c9152ff
cakevulture <grayfox8597@gmail.com>
2018-10-31 16:53:43 -0400

    Added kanban view

--- models/models.py
+++ models/models.py
@@ -45,6 +45,7 @@ class Session(models.Model):
 	duration = fields.Float(digits=(6, 2), help="Duration in days")
 	seats = fields.Integer(string="Number of seats")
 	active = fields.Boolean(default=True)
+	color = fields.Integer()
 
 	instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
 	course_id = fields.Many2one('openacademy.course',
@@ -53,6 +54,8 @@ class Session(models.Model):
 
 	taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 	end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
+	attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
 
 	@api.depends('seats', 'attendee_ids')
 	def _taken_seats(self):
@@ -104,6 +107,11 @@ class Session(models.Model):
 			start_date = fields.Datetime.from_string(r.start_date)
 			end_date = fields.Datetime.from_string(r.end_date)
 			r.duration = (end_date - start_date).days + 1
+
+	@api.depends('attendee_ids')
+	def _get_attendees_count(self):
+		for r in self:
+			r.attendees_count = len(r.attendee_ids)
 
 	@api.constrains('instructor_id', 'attendee_ids')
 	def _check_instructor_not_in_attendees(self):
